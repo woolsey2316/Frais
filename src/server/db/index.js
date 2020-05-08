237df49116ab25d8aa5bfadc79d213769ae0759d
David Woolsey <woolsey2316@gmail.com>
2020-05-08 14:50:13 +1000

    first commit

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/productsdb', { useNewUrlParser: true })
+    .connect( process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/productsdb', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
